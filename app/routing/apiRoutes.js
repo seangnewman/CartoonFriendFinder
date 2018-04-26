25eac3e1cdaea54a7bd4fca5710430f76b255194
Sean Newman <seangnewman@msn.com>
2018-04-25 18:12:48 -0700

    trouble with css

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,57 @@
+// ===============================================================================
+// LOAD DATA
+// These are the controllers in ASP/CORE
+// Although by convention a separate controller is created for each source
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var charData = require("../data/friends");
+ 
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/survey", function(req, res) {
+    res.json(charData);
+  });
+ 
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the tableData array)
+  // ---------------------------------------------------------------------------
+
+  app.post("/api/survey", function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body-parser middleware
+    
+      charData.push(req.body);
+      res.json(true);
+     
+  });
+
+  // ---------------------------------------------------------------------------
+  // I added this below code so you could clear out the table while working with the functionality.
+  // Don"t worry about it!
+
+  app.post("/api/clear", function() {
+    // Empty out the arrays of data
+    charData = [];
+    
+
+    console.log(charData);
+  });
+};
